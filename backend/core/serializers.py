b9c2d2e0aa4073a81623d7bd1acb4912e1f1f7e6
JasonTinotenda <kufakwatenziyason@gmail.com>
2024-08-13 17:47:47 +0200

    basic backend tests:-success

---
+++ backend/core/serializers.py
@@ -0,0 +1,10 @@
+
+from rest_framework import serializers
+from .models import Core
+
+
+class CoreSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Core
+        fields = ['id', 'name', 'description']
--- backend/core/views.py
+++ backend/core/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import permissions
+from .models import Core
+from .serializers import CoreSerializer
 
-# Create your views here.
+
+class CoreApiView(APIView):
+
+    # permission_classes = [permissions.AllowAny]
+
+    def get(self, request, *args, **kwargs):
+        cores = Core.objects.all()
+        serializer = CoreSerializer(cores, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def post(self, request, *args, **kwargs):
+        serializer = CoreSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
