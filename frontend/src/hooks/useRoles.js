a2a82b64dc131fe5c4cc37b91237ecd2d519b59c
JasonTinotenda <kufakwatenziyason@gmail.com>
2024-08-17 22:34:37 +0200

    frontend reset

--- frontend/src/hooks/useRoles.js
+++
@@ -1,6 +0,0 @@
-import { useSelector } from 'react-redux';
-
-export const useRole = () => {
-  const { user } = useSelector(state => state.auth);
-  return user?.role || 'user'; // Default role to 'user' if not available
-};
